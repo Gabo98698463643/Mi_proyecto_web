cc12bc15f7fbe4d6226eff1006f124470263201c
Gabriel A <gaboa.p@hotmail.com>
2021-11-14 20:17:11 -0600

    commit -select escrito sin ids-

---
+++ Proyecto_web/mi_proyecto_web.js
@@ -0,0 +1,71 @@
+
+/*
+1- Abeja 
+2- Arapaima
+3- Ardilla
+4- Arpía
+5- Avispón
+6- Babosa
+7- Búho
+8- Bulbo Mohoso
+9- Cabeza meteorito
+10- Cara monstruo
+11- Cargador de hormigas león
+12- Carmebicho
+13- Criatura de pared
+14- Crisopa prismática
+15- Diablillo de fuego
+16- Diente de león enfadado
+17- Doctor látigo
+18- Duende arquero
+19- Duede explorador
+20- Duende guerrero
+21- Duende hechicero
+22- Duende ladrón
+23- Duende peón
+24- Enjambre de hormigas león
+25- Excavador
+26- Flufy
+27- Globo ventoso
+28- Gnomo
+29- Golem de granito
+30- Granito elemental
+31- Guillermo
+32- Gusano gigante
+33- Murciélago de hielo
+34- Nimbo enfadado
+35- Ojo errante
+36- Pilar de nebulosa
+37- Pilar de stardust
+38- Pilar de vortex
+39- Pilar solar
+40- Pinky 
+41- Serpiente de hueso
+42- Slime (Halloween)
+43- Trepasangre
+44- Visión antigua
+45- (J) Abeja reina
+46- (J) Cerebro de Cthulhu
+47- (J) Devoramundos
+48- (J) Duke Fishron
+49- (J) El Destructor
+50- (J) El Dios antorcha
+51- (J) Emperatriz de la luz
+52- (J) Espasmatizor
+53- (J) Esqueletrón
+54- (J) Esqueletrón Prime
+55- (J) Gólem
+56- (J) Muro de Carne
+57- (J) Ojo de Cthulhu
+58- (J) Plantera
+59- (J) Retinator
+60- (J) Rey slime
+61- (J) Sectario Lunático
+62- (F) Señor de la luna
+
+Abeja si tiene bestiario
+Crisopa igual tiene bastiario
+Dietne de leon tiene bastiario
+
+*/
+
